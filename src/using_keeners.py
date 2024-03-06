052b7ed9b61341ffea853f20df56f693a3d38272
Ana Erthal <anaerthal@MacBook-Pro-de-Ana.local>
2024-03-05 22:00:01 -0300

    clean repo

--- src/using_keeners.py
+++ src/using_keeners.py
@@ -2,30 +2,29 @@ from models import *
 import pandas as pd
 
 # read data and choose class
-data = pd.read_excel('./data/final_data.xlsx')
-data = data[data['Classe Vela'] == 'Ilca 6']
+data = pd.read_excel('../data/final_data.xlsx')
+data = data[data['Classe Vela'] == '49erFX']
 
 for competition in data['Nome Competição']:
     data.loc[data['Nome Competição'] == competition, 'Ano'] = int(re.findall(r'\d{4}', competition)[0])
 
-# split train/test 
-train = data[~data['Nome Competição'].isin(['Trofeo S.A.R Princesa Sofia 2023', 'World Championship 2023'])]
-data = train
+# split train/test if needed
+#train = data[~data['Nome Competição'].isin(['Trofeo S.A.R Princesa Sofia 2023', 'World Championship 2023'])]
+#data = train
 
 keeners = Keeners()
 keeners.fit(data, time_decay=False, method='alpha')
 ratings = keeners.ratings
 
 ratings = pd.DataFrame.from_dict(ratings, orient='index', columns=['Rating'])
-ratings.to_csv('./rankings/keenersalpha-49er-trainset.csv')
-
+ratings.to_csv('../rankings/keeners_alpha_49erFX.csv')
 
 keeners = Keeners()
 keeners.fit(data, time_decay=True, method='alpha')
 ratings = keeners.ratings
 
 ratings = pd.DataFrame.from_dict(ratings, orient='index', columns=['Rating'])
-ratings.to_csv('./rankings/keenersalphatime-49er-trainset.csv')
+ratings.to_csv('../rankings/keeners_alpha_time_49erFX.csv')
 
 
 keeners = Keeners()
@@ -33,7 +32,7 @@ keeners.fit(data, time_decay=False, method='beta')
 ratings = keeners.ratings
 
 ratings = pd.DataFrame.from_dict(ratings, orient='index', columns=['Rating'])
-ratings.to_csv('./rankings/keenersbeta-49er-trainset.csv')
+ratings.to_csv('../rankings/keeners_beta_49erFX.csv')
 
 
 keeners = Keeners()
@@ -41,4 +40,4 @@ keeners.fit(data, time_decay=True, method='beta')
 ratings = keeners.ratings
 
 ratings = pd.DataFrame.from_dict(ratings, orient='index', columns=['Rating'])
-ratings.to_csv('./rankings/keenersbetatime-49er-trainset.csv')
+ratings.to_csv('../rankings/keeners_beta_time_49erFX.csv')
